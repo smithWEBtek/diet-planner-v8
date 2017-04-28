2e1441f25fa386d8b6f2fa363a98105e1a0ed0c3
smithwebtek <brad@smithwebtek.com>
2017-04-28 12:42:16 -0400

    user controller and model, commenting out code to get basic user working with devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,70 @@
 class UsersController < ApplicationController
+  # before_action :authorize_admin, only: [:destroy]
+  # before_action :set_user, only: [:export_csv, :show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  # before_action :authenticate_user!, only: [:show]
+  # before_action :load_models
+
+  def new
+    @user = User.new
+    # authorize_admin
+  end
+
+  # def export_csv
+  #   render '/users/user_data.csv.erb'
+  # end
+
+  def show
+    # @user = current_user unless current_user.admin?
+    # @meals_count = @user.meals.count
+    # @user_cals = @user.user_cals
+    # @user_avg_cals_per_meal = @user.user_avg_cals_per_meal
+    # load_diet_stats
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      flash[:notice] = "Welcome, #{@user.username.upcase}! you have successfully signed up, please SIGN IN."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user
+    # @log = @user.logs.build
+  end
+
+  def update
+    @user.update_without_password(user_params)
+    if @user.save
+      flash[:notice] = 'User Account updated.'
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.delete
+      flash[:notice] = 'User deleted'
+      redirect_to root_path
+    else
+      flash[:notice] = @user.errors.full_messages
+      redirect_to user_path(@user)
+    end
+  end
+
+  private
+	  def set_user
+	    @user = User.find_by_id(params[:id])
+	  end
+
+	  def user_params
+	    params.require(:user).permit(:username, :role, :email, :password, :weight, :diet_id)
+	      # logs_attributes: [:user_id, :date, :note],
+	    # meals_attributes: [:user_id, :mealdate, :mealname_id, :food_id, :new_food, :qty, :note])
+	  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,108 @@
-class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+class User < ApplicationRecord
+  enum role: [:standard, :admin]
+  after_initialize :set_default_role, if: :new_record?
 
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable,
+  :omniauthable, omniauth_providers: [:facebook]
+
+  # has_many :meals
+  # has_many :foods, through: :meals
+  # belongs_to :diet
+  # has_many :logs
+
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
+
+  # scope :undieters, -> { where(diet_id: '1') }
+  # scope :vegans, -> { where(diet_id: '2') }
+  # scope :lowcarbers, -> { where(diet_id: '3') }
+  # scope :balancers, -> { where(diet_id: '4') }
+  # scope :fishatarians, -> { where(diet_id: '5') }
+  # scope :carnivores, -> { where(diet_id: '6') }
+  # scope :lumberjacks, -> { where(diet_id: '7') }
+  # scope :vampires, -> { where(diet_id: '8') }
+  # scope :junkers, -> { where(diet_id: '9') }
+ 
+  def set_default_role
+    self.role ||= :standard
+  end
+  
+  # def self.group_cals(group)
+  #   cals = []
+  #   group.each do |user|
+  #     user.meals.each do |meal|
+  #       cals.push(meal.food.cals)
+  #     end
+  #   end
+  #   cals.inject(0) { |sum, x| sum + x }
+  # end
+
+  # def self.group_meals_count(group)
+  #   meals_count = []
+  #   group.each do |user|
+  #     meals_count.push(user.meals.count)
+  #   end
+  #   meals_count.inject(0) { |sum, x| sum + x }
+  # end
+
+  # def self.avg_cals_per_meal(group)
+  #   group_cals(group) / group_meals_count(group) if group_meals_count(group) > 0
+  # end
+
+  # def user_cals
+  #   cals = []
+  #   meals.each do |meal|
+  #     meal_cals = meal.qty * meal.food.cals
+  #     cals.push(meal_cals)
+  #   end
+  #   cals.inject(0) { |sum, x| sum + x }
+  # end
+
+  # def user_avg_cals_per_meal
+  #   user_cals / meals.count if meals.count > 0
+  # end
+
+
+  # def self.from_omniauth(user_data)
+  #   where(provider: user_data.provider, uid: user_data.uid).first_or_create do |user|
+  #     user.uid = user_data.uid
+  #     user.email = user_data.info.email
+  #     user.password = Devise.friendly_token[0, 20]
+  #     user.username = user_data.info.name
+  #     user.image = user_data.info.image
+  #   end
+  # end
+
+  # def join_date
+  #   created_at.to_date
+  # end
+
+  # def meals_attributes=(meals_attributes)
+  #   meals_attributes.each do |_i, meal_attributes|
+  #     if meal_attributes[:food_id].present?
+  #       meals.create(meal_attributes)
+  #     elsif meal_attributes[:new_food] != ''
+  #       food = Food.find_or_create_by(name: meal_attributes[:new_food])
+  #       meal_attributes[:food_id] = food.id
+  #       meal = meals.create(meal_attributes)
+  #       meal.new_food = ''
+  #       meal.save
+  #     else
+  #       errors.full_messages
+  #     end
+  #   end
+  # end
+
+  # def logs_attributes=(logs_attributes)
+  #   logs_attributes.values.each do |log_attributes|
+  #     self.logs.build(log_attributes)
+  #   end
+  # end
+
+  private
+
+  def self.admin?
+    !!self.role == 'admin'
+  end
 end
