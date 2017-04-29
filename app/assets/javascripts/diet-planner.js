93c86140bb31199efe77d5d204ae94f50006432c
smithwebtek <brad@smithwebtek.com>
2017-04-29 15:59:46 -0400

    build views for celeb, fix controller, test basic form works, before ajaxify

--- app/assets/javascripts/diet-planner.js
+++ app/assets/javascripts/diet-planner.js
@@ -1,17 +1,17 @@
-// $(function(){
-//   $("#new_celeb_form").on("submit", function(e){
+$(function(){
+  $("#new_celeb_form").on("submit", function(e){
  
-//   	// $.ajax(
-//   	// 	method: "POST"
-//   	// 	URL: '/quotes'
-//   	// 	data: $("#quick_quote_form").val
-//  console.log(this)
-//  debugger;
-//   //   $.get(this.href).success(function(response){
-//   //     $("div#ajax-contacts").html(response);
-//   //   }).error(function(){
-//   //     alert("we broke!!!");
-//   //   });
-//     e.preventDefault();
-//   });
-// }); 
+  	// $.ajax(
+  	// 	method: "POST"
+  	// 	URL: '/quotes'
+  	// 	data: $("#quick_quote_form").val
+ console.log(this)
+ debugger;
+  //   $.get(this.href).success(function(response){
+  //     $("div#ajax-contacts").html(response);
+  //   }).error(function(){
+  //     alert("we broke!!!");
+  //   });
+    e.preventDefault();
+  });
+}); 
--- app/controllers/celebs_controller.rb
+++ app/controllers/celebs_controller.rb
@@ -1,2 +1,70 @@
 class CelebsController < ApplicationController
+  before_action :authorize_admin, only: [:edit, :update, :destroy]
+  before_action :set_celeb, only: [:show, :edit, :update, :destroy]
+  before_action :load_models
+
+  def index
+    @celebs = Celeb.all
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @celebs }
+    end
+  end
+  
+  def show
+     respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @celeb }
+    end
+  end
+
+  def new
+    @celeb = Celeb.new
+  end
+
+  def create
+    @celeb = Celeb.new(celeb_params)
+    if @celeb.save
+      flash[:notice] = "celeb created."
+      respond_to do |format|
+      	format.html {  redirect_to celeb_path(@celeb) }
+      	format.json { render json: @celeb }
+    end
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @celeb.update(celeb_params)
+    if @celeb.save
+      flash[:notice] = "celeb updated."
+      redirect_to celeb_path(@celeb)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @Celeb.delete
+      flash[:notice] = 'Celeb deleted'
+      redirect_to user_Celebs_path(@Celeb.user)
+    else
+      flash[:notice] = 'Celeb NOT deleted'
+      redirect_to user_Celebs_path(current_user)
+    end
+  end
+
+  private
+    def set_celeb
+      @celeb = Celeb.find_by_id(params[:id])
+    end
+
+    def celeb_params
+      params.require(:celeb).permit(:name)
+    end
 end
+
