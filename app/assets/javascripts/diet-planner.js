757eb67ae2bf8629914e46ef21cfd26159504990
smithwebtek <brad@smithwebtek.com>
2017-04-29 10:03:53 -0400

    basic js file, test

---
+++ app/assets/javascripts/diet-planner.js
@@ -0,0 +1,11 @@
+$(function(){
+  // $("a.load_contacts").on("click", function(e){
+  //   $.get(this.href).success(function(response){
+  //     $("div#ajax-contacts").html(response);
+  //   }).error(function(){
+  //     alert("we broke!!!");
+  //   });
+  //   e.preventDefault();
+  // });
+  console.log("js is working")
+}); 
