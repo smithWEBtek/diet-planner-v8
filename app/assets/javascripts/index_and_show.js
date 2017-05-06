807376fe42633b179a8c27003b2f3bc00cad437a
smithwebtek <brad@smithwebtek.com>
2017-05-06 15:50:45 -0400

    getting meal detail working, more to come

--- app/assets/javascripts/index_and_show.js
+++ app/assets/javascripts/index_and_show.js
@@ -17,6 +17,15 @@ function clearIndexAndShowArea(){
 	$("#groups").html("");
 	$("#diets").html("");
 	$("#user_show").html("");
+	$("#user_show_meals").html("");
+	$("#user_show_foods").html("");
+	$("#user_show_logs").html("");
+	$("#user_show_id").html("");
+	$("#user_show_username").html("");
+	$("#user_show_role").html("");
+	$("#user_show_email").html("");
+	$("#user_show_weight").html("");
+	$("#user_show_diet").html("");
 }
 
 function usersIndex(){
@@ -55,17 +64,15 @@ function userShow(id){
 			dataType: 'json',
 			success: function(response){
 				clearIndexAndShowArea();
-				
 				var id = response.id; 
 				var username = response.username;
 				var role = response.role;
 				var email = response.email; 
 				var weight = response.weight; 
-				var diet = response.diet; 
-
-				// var meals = response.meals; 
-				// var foods = response.foods; 
-				// var logs = response.logs;
+				var diet = response.diet;
+				var meals = response.meals; 
+				var foods = response.foods;
+				var logs = response.logs;
 			
 				$("#user_show_id").html("ID: " + id);
 				$("#user_show_username").html("Username: " + username);
@@ -74,17 +81,26 @@ function userShow(id){
 				$("#user_show_weight").html("Weight: " + weight);
 				$("#user_show_diet").html("Diet: " + diet.name);
 
-				// for (var i = meals.length - 1; i >= 0; i--) {
-				// $("#user_show_meals").append(meals[i]);
-				// }
+				$("#user_show_meals").append("<hr>" + "MEALS:" + "<br>");
+					for (var i = meals.length - 1; i >= 0; i--) {
 
-				// for (var i = foods.length - 1; i >= 0; i--) {
-				// $("#user_show_foods").append(foods[i])
-				// }
-
-				// for (var i = logs.length - 1; i >= 0; i--) {
-				// $("#user_show_logs").append(logs[i])	
-				// }
+					var mealdate = meals[i].date
+					var mealname = meals[i].mealname.name;
+					var mealfood = meals[i].food.name;
+					var mealqty = meals[i].meal.qty;
+					var mealcals = meals[i].meal.cals;
+					var meal = mealdate + ": for " + mealname + ", I had " + mealqty + mealfood + "'s, and @ " + mealcals + "per " + mealfood + ", it means I added " + mealqty * mealcals + "to my belly!"
+					$("#user_show_meals").append(meal);
+					}
+				$("#user_show_foods").append("<hr>" + "FOODS:" + "<br>");
+					for (var i = foods.length - 1; i >= 0; i--) {
+					$("#user_show_foods").append(foods[i].name + "<br>")
+					}
+				$("#user_show_logs").append("<hr>" + "LOGS:" + "<br>");
+					for (var i = logs.length - 1; i >= 0; i--) {
+					$("#user_show_logs").append(logs[i].note + "<br>")	
+					}
+				$("#user_show_logs").append("<hr>");
 			}
 		})
 	})
--- app/serializers/meal_serializer.rb
+++ app/serializers/meal_serializer.rb
@@ -1,5 +1,5 @@
 class MealSerializer < ActiveModel::Serializer
-  attributes :id, :mealdate, :mealname_id, :user_id, :food_id, :new_food , :qty, :note
+  attributes :id, :mealdate, :mealname, :mealname_id, :user_id, :food_id, :new_food , :qty, :note
   belongs_to :user
   belongs_to :mealname
   belongs_to :food
