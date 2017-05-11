90ea612daad19c3b9e2587ca970efd396d178bb2
smithwebtek <brad@smithwebtek.com>
2017-05-11 19:23:17 -0400

    refactor CelebrityMeal

--- app/assets/javascripts/quotes.js
+++ app/assets/javascripts/quotes.js
@@ -139,6 +139,19 @@ function newDropdownQuote(){
 		});
 	});
 };
+
+function CelebrityMeal(celeb, verb, adj, food, diet, phrase){
+	this.celeb = celeb;
+	this.verb = verb;
+	this.adj = adj;
+	this.food = food;
+	this.diet = diet;
+	this.phrase = phrase;
+}
+
+CelebrityMeal.prototype.customQuote = function(){
+	return ("Using the customQuote method, on the class CelebrityMeal..." + this.celeb + " " + this.verb + " " + this.adj + " " + this.food + " while on the " + this.diet + " diet, and said, \"" + this.phrase + "\"" + ".");	
+}
  
 function newCelebMeal(){
 	$("#new_celeb_meal_button").on("click", function(e) {
