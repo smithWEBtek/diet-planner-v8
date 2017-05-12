289ccb3cf48cb771cf54bf5728d4e12bf4e734a4
smithwebtek <brad@smithwebtek.com>
2017-05-12 11:27:24 -0400

    trying to get app layout consistent

--- app/assets/javascripts/index_and_show.js
+++ app/assets/javascripts/index_and_show.js
@@ -1,5 +1,4 @@
 $(function(){
-	clearIndexAndShowAreas();
 	usersIndex();
 	userShow();
 	foodsIndex();
@@ -13,31 +12,12 @@ $(function(){
 	mealnamesIndex();
 })
 
-function clearIndexAndShowAreas(){
-	$("#users").html("");
-	$("#foods").html("");
-	$("#meals").html("");
-	$("#mealnames").html("");
-	$("#groups").html("");
-	$("#diets").html("");
-	$("#user_show").html("");
-	$("#user_show_meals").html("");
-	$("#user_show_foods").html("");
-	$("#user_show_logs").html("");
-	$("#user_show_id").html("");
-	$("#user_show_username").html("");
-	$("#user_show_role").html("");
-	$("#user_show_email").html("");
-	$("#user_show_weight").html("");
-	$("#user_show_diet").html("");
-}
-
 function usersIndex(){
-	$("span#users_index").on('click', function(e){
-		clearIndexAndShowAreas();
+	$("#users_index").on('click', function(e){
+		$("#app-container").html("");
 		e.stopImmediatePropagation();
 		e.preventDefault();
-		history.replaceState(null, null, "/users")
+		// history.replaceState(null, null, "/users")
 	$.ajax({
 		type: 'get',
 		url: '/api_users.json',
@@ -50,7 +30,7 @@ function usersIndex(){
 			var weight = response[i].weight;
 			var diet = response[i].diet.name;
 			var user = username + "  "+ email+ "  "+ weight + "lbs " + "   " + diet + "  <br>";			
-			$("#users").append("<p data-id=" + id + ">" + user + "</p>");
+			$("#app-container").append("<p data-id=" + id + ">" + user + "</p>");
 				}
 		userShow(id);
 			}
@@ -60,16 +40,16 @@ function usersIndex(){
 
 function userShow(id){
 	$("#users p").on("click", function(e){
-		clearIndexAndShowAreas();
+		$("#app-container").html("");
 		var id = parseInt(this.dataset.id);
-		history.replaceState(null, null,"/users/" + id)
+		// history.replaceState(null, null,"/users/" + id)
 		e.preventDefault();
 		$.ajax({
 			type: 'get',
 			url: '/users/' + id, 
 			dataType: 'json',
 			success: function(response){
-				clearIndexAndShowAreas();
+				$("#app-container").html("");
 				var id = response.id; 
 				var username = response.username;
 				var role = response.role;
@@ -108,7 +88,7 @@ function userShow(id){
 					for (var i = logs.length - 1; i >= 0; i--) {
 					$("#user_show_logs").append(logs[i].note + "<br>")	
 					}
-				$("#user_show_logs").append("<hr>");
+				$("#app-container").append("<hr>");
 			}
 		})
 	})
@@ -116,22 +96,22 @@ function userShow(id){
 
 function foodsIndex(){
 	$("span#foods_index").on("click", function(e){
-		clearIndexAndShowAreas();
+		$("#app-container").html("");
 		e.stopImmediatePropagation();
 		e.preventDefault();
-		history.replaceState(null, null, "/foods")
-	$.ajax({
-		type: 'get',
-		url: '/api_foods.json',
-		dataType: 'json',
-		success: function(response){
-			for (var i = response.length - 1; i >= 0; i--) {
-				var id = response[i].id;
-				var name = response[i].name;
-				var group = response[i].group.name;
-				var food = name + "  "+ group + "<br>";
-				$("#foods").append("<p data-id=" + id + ">" + food + "</p>");
-		foodShow();
+		// history.replaceState(null, null, "/foods")
+		$.ajax({
+			type: 'get',
+			url: '/api_foods.json',
+			dataType: 'json',
+			success: function(response){
+				for (var i = response.length - 1; i >= 0; i--) {
+					var id = response[i].id;
+					var name = response[i].name;
+					var group = response[i].group.name;
+					var food = name + "  "+ group + "<br>";
+					$("#app-container").append("<p data-id=" + id + ">" + food + "</p>");
+					foodShow();
 				}
 			}
 		});
@@ -140,20 +120,20 @@ function foodsIndex(){
 
 function foodShow(){
 	$("#foods p").on("click", function(e){
-		clearIndexAndShowAreas();
+		$("#app-container").html("");
 		var id = parseInt(this.dataset.id);
-		history.replaceState(null, null,"/foods/" + id)
-			e.stopImmediatePropagation();
+		// history.replaceState(null, null,"/foods/" + id)
+		e.stopImmediatePropagation();
 		e.preventDefault();
 		$.ajax({
 			type: 'get',
 			url: '/foods/' + id, 
 			dataType: 'json',
 			success: function(response){
-				clearIndexAndShowAreas();
+			$("#app-container").html("");
 				var id = response.id; 
 				var foodname = response.name;
-				$("#food_show").html("ID: " + id + "<br> Name: " + foodname);
+				$("#app-container").html("ID: " + id + "<br> Name: " + foodname);
 			}
 		})
 	})
@@ -161,20 +141,20 @@ function foodShow(){
 
 function mealsIndex(){
 	$("span#meals_index").on("click", function(e){
-		clearIndexAndShowAreas();
+		$("#app-container").html("");
 		e.stopImmediatePropagation();
 		e.preventDefault();
-		history.replaceState(null, null, "/meals")
-	$.ajax({
-		type: 'get',
-		url: '/api_meals.json',
-		dataType: 'json',
-		success: function(response){
-		for (var i = response.length - 1; i >= 0; i--) {
-			var mealname = response[i].mealname.name;
-			var food = response[i].food.name;
-			var meal = mealname + ": " + food + "<br>";
-			$("#meals").append(meal);
+		// history.replaceState(null, null, "/meals")
+		$.ajax({
+			type: 'get',
+			url: '/api_meals.json',
+			dataType: 'json',
+			success: function(response){
+			for (var i = response.length - 1; i >= 0; i--) {
+				var mealname = response[i].mealname.name;
+				var food = response[i].food.name;
+				var meal = mealname + ": " + food + "<br>";
+				$("#app-container").append(meal);
 				}
 			}
 		});
@@ -183,17 +163,17 @@ function mealsIndex(){
 
 function mealnamesIndex(){
 	$("span#mealnames_index").on("click", function(e){
-		clearIndexAndShowAreas();
+		$("#app-container").html("");
 		e.stopImmediatePropagation();
 		e.preventDefault();
-		history.replaceState(null, null, "/mealnames")
-	$.ajax({
-		type: 'get',
-		url: '/api_mealnames',
-		dataType: 'json',
-		success: function(response){
-		for (var i = response.length - 1; i >= 0; i--) {
-			$("#mealnames").append(response[i].name + "<br>");
+		// history.replaceState(null, null, "/mealnames")
+		$.ajax({
+			type: 'get',
+			url: '/api_mealnames',
+			dataType: 'json',
+			success: function(response){
+			for (var i = response.length - 1; i >= 0; i--) {
+				$("#app-container").append(response[i].name + "<br>");
 				}
 			}
 		});
@@ -202,17 +182,17 @@ function mealnamesIndex(){
 
 function groupsIndex(){
 	$("span#groups_index").on("click", function(e){
-		clearIndexAndShowAreas();
+		$("#app-container").html("");
 		e.stopImmediatePropagation();
 		e.preventDefault();
-		history.replaceState(null, null, "/groups")
-	$.ajax({
-		type: 'get',
-		url: '/api_groups',
-		dataType: 'json',
-		success: function(response){
-		for (var i = response.length - 1; i >= 0; i--) {
-			$("#groups").append(response[i].name + "<br>");
+		// history.replaceState(null, null, "/groups")
+		$.ajax({
+			type: 'get',
+			url: '/api_groups',
+			dataType: 'json',
+			success: function(response){
+			for (var i = response.length - 1; i >= 0; i--) {
+				$("#app-container").append(response[i].name + "<br>");
 				}
 			}
 		});
@@ -221,17 +201,17 @@ function groupsIndex(){
 
 function dietsIndex(){
 	$("span#diets_index").on("click", function(e){
-		clearIndexAndShowAreas();
+		$("#app-container").html("");
 		e.stopImmediatePropagation();
 		e.preventDefault();
-		history.replaceState(null, null, "/diets")
-	$.ajax({
-		type: 'get',
-		url: '/api_diets',
-		dataType: 'json',
-		success: function(response){
-		for (var i = response.length - 1; i >= 0; i--) {
-			$("#diets").append(response[i].name + "<br>");
+		// history.replaceState(null, null, "/diets")
+		$.ajax({
+			type: 'get',
+			url: '/api_diets',
+			dataType: 'json',
+			success: function(response){
+			for (var i = response.length - 1; i >= 0; i--) {
+				$("#app-container").append(response[i].name + "<br>");
 				}
 			}
 		});
