036683cbc278bf795e887abd297d631061308e5c
smithwebtek <brad@smithwebtek.com>
2020-11-17 19:50:51 -0500

    Update deploy configs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,8 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "diet-planner.smithwebtek.com", user: "deploy", roles: %w{app db web}
+# server "diet-planner.smithwebtek.com", user: "deploy", roles: %w{app db web}
+server "67.205.165.187", user: "deploy", roles: %w{app db web}
 
 # role-based syntax
 # ==================
